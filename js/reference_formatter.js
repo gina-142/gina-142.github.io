659763b23daaae13f334a4b3a8e0eeae5252b1fe
Gina <ginaelliott4242@gmail.com>
2026-02-09 22:14:55 +0000

    json update

---
+++ js/reference_formatter.js
@@ -0,0 +1,20 @@
+function getReferences(reference_set) {
+    fetch("references.json")
+    .then(res => res.json())
+    .then(data => {
+        const references = document.getElementById("references");
+
+        data.reference_set.forEach((event, index) => {
+            const content = document.createElement("div");
+            content.className = "reference";
+
+            const p = document.createElement("p");
+            p.className = "reference_text";
+            p.innerHTML = `<b>[${index+1}]</b>   ${event.authors}, "${event.paper_title}" <i>${event.journal_name}</i>, <b>${event.volume}</b>, ${event.issue}, (${event.year}), doi: ${event.doi}.`;
+
+            content.appendChild(p);
+            references.appendChild(content)
+        });
+    })
+    .catch(err => console.error(err));
+};
--- js/star_trek_script.js
+++ js/star_trek_script.js
@@ -1,20 +1,6 @@
 console.log("JavaScript is connected!");
 
-fetch("references.json")
-    .then(res => res.json())
-    .then(data => {
-        const references = document.getElementById("references");
+console.log("Above get references");
 
-        data.star_trek_refs.forEach((event, index) => {
-            const content = document.createElement("div");
-            content.className = "reference";
-
-            const p = document.createElement("p");
-            p.className = "reference_text";
-            p.innerHTML = `<b>[${index+1}]</b>   ${event.authors}, "${event.paper_title}" <i>${event.journal_name}</i>, <b>${event.volume}</b>, ${event.issue}, (${event.year}), doi: ${event.doi}.`;
-
-            content.appendChild(p);
-            references.appendChild(content)
-        });
-    })
-    .catch(err => console.error(err));
+const star_trek = JSON.parse("references");
+getReferences(star_trek);
